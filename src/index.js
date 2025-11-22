a4650ef71a7d6e0d8a92f55e0586697dd555f66e
Shashank Ranjan <shashankranjan07@gmail.com>
2025-11-22 15:36:08 +0530

    Updated CORS policy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
+
 import dotenv from "dotenv";
+dotenv.config();
+
 import connectDB from "./utils/db.js";
 import taskRouter from "./routes/task.route.js";
-
+import cors from "cors";
 
 const app = express();
-dotenv.config();
+
+app.use(cors ({
+    origin: "https://sentinel.raashah.me",
+    credentials: true
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +23,8 @@ app.get('/', (req, res) => {
     res.send("Hellow World!")
 })
 
-app.listen(process.env.PORT, () => {
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
-    connectDB();
-})
+  });
+});
